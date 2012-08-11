9499aeb52b05617fd779f0d3b636b42b9c197568
Trip Leonard <tripleonard@gmail.com>
2012-08-11 00:05:08 -0700

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-todo-mysql
-==========
+A Todo List Manager as a MySQL Database
+=======================================
 
-A todo list manager built out of a MySQL database
+Are you a MySQL DBA or have the mysql client open all the time? Do you want a cross platform todo list manager?  This (waste of time?) may be for you.
+
+Installation
+------------
+
+	mysql -u username -p -h localhost < todo.my.sql
+
+Usage
+-----
+
+Start MySQL client
+
+	mysql -u username -p
+	
+	use todo;
+
+To add a new todo
+	
+	 ('buy milk',1,'errand');
+
+To list todo by priority
+	
+	call list;
+	+----+-------------------------------+----------+----------+---------+---------------------+
+	| id | todo                          | priority | context  | project | date_created        |
+	+----+-------------------------------+----------+----------+---------+---------------------+
+	|  1 | get sql queries from backup   |        1 | computer | NULL    | 2012-08-04 06:27:18 |
+	|  3 | order good to go card         |        1 | computer | NULL    | 2012-08-04 06:27:18 |
+	|  4 | write thankyou                |        1 | home     | NULL    | 2012-08-04 06:27:18 |
+	|  5 | sched eye exam for frankie    |        2 | call     | NULL    | 2012-08-09 15:03:59 |
+	|  6 | buy Scott gift card thank you |        2 | errand   | NULL    | 2012-08-09 15:09:36 |
+	|  9 | organized DVD collection      |        4 | home     | NULL    | 2012-08-09 16:29:27 |
+	| 10 | scan notebook in to pdf       |        4 | home     | NULL    | 2012-08-09 16:30:37 |
+	+----+-------------------------------+----------+----------+---------+---------------------+
+	
+To list all todos with a specific context
+	
+	call context('errand');
+	+----+-------------------------------+----------+----------+---------+---------------------+
+	| id | todo                          | priority | context  | project | date_created        |
+	+----+-------------------------------+----------+----------+---------+---------------------+
+	|  6 | buy Scott gift card thank you |        2 | errand   | NULL    | 2012-08-09 15:09:36 |
+	+----+-------------------------------+----------+----------+---------+---------------------+
+
+
+To list all todos for a specific project
+	
+	call project('launch');
+	
+
+
---
+++ todo.my.sql
@@ -0,0 +1,158 @@
+/* a personal mysql todo */
+
+DROP database IF EXISTS todo;
+CREATE database todo;
+
+USE todo;
+
+DROP TABLE IF EXISTS list;
+CREATE TABLE list(
+	id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	todo text,
+	priority smallint,
+	context varchar(30),
+	project varchar(30),
+	date_created timestamp NOT NULL
+	)ENGINE=InnoDB;
+	
+DROP TABLE IF EXISTS done;
+CREATE TABLE done(
+	id int,
+	todo text,
+	priority smallint,
+	context varchar(30),
+	project varchar(30),
+	date_created datetime,
+	date_completed timestamp NOT NULL
+	)ENGINE=InnoDB;
+
+/* create a new todo */
+
+DROP PROCEDURE IF EXISTS new;
+
+DELIMITER $$
+
+CREATE PROCEDURE new(
+	todo_in varchar(100),
+	priority_in smallint,
+	context_in varchar(30),
+	project_in varchar(30))
+	
+BEGIN
+	
+	INSERT INTO list(todo,priority,context,project)
+	VALUES(todo_in,priority_in,context_in,project_in);
+
+END;
+
+$$
+
+DELIMITER ;
+	
+/* list todos order by priority */
+
+DROP PROCEDURE IF EXISTS list;
+
+DELIMITER $$
+
+CREATE PROCEDURE list()
+BEGIN
+	
+	SELECT id,todo,priority,context,project
+	FROM list
+	ORDER BY priority;
+
+END;
+
+$$
+
+DELIMITER ;
+
+/* list todos with a particular context */
+
+DROP PROCEDURE IF EXISTS context;
+
+DELIMITER $$
+
+CREATE PROCEDURE context(context_in varchar(30))
+BEGIN
+	
+	SELECT todo,priority,context,project
+	FROM list
+	WHERE context=context_in;
+
+END;
+
+$$
+
+DELIMITER ;
+
+/* list todos for a particular project */
+
+DROP PROCEDURE IF EXISTS project;
+
+DELIMITER $$
+
+CREATE PROCEDURE project(project_in varchar(30))
+BEGIN
+	
+	SELECT todo,priority,context,project
+	FROM list
+	WHERE context=project_in;
+
+END;
+
+$$
+
+DELIMITER ;
+	
+/* move completed task to done table and delete from list */
+
+DROP PROCEDURE IF EXISTS do;
+
+DELIMITER $$
+
+CREATE PROCEDURE do(id_in int)
+BEGIN
+	DECLARE ListId_v int;
+	DECLARE Todo_v text;
+	DECLARE Priority_v smallint;
+	DECLARE Context_v varchar(30);
+	DECLARE Project_v varchar(30);
+	DECLARE DateCreated_v datetime;
+	
+	SELECT id,todo,priority,context,project,date_created INTO ListId_v,Todo_v,Priority_v,Context_v,Project_v,DateCreated_v
+	FROM todo.list
+	WHERE id_in=id;
+
+	INSERT INTO done(id,todo,priority,context,project,date_created)
+	VALUES (ListId_v,Todo_v,Priority_v,Context_v,Project_v,DateCreated_v);
+	
+	DELETE FROM list
+	WHERE id_in=list.id;
+
+END;
+
+$$
+
+DELIMITER ;
+
+/* list last 20 completed todos ascending */
+
+DROP PROCEDURE IF EXISTS done;
+
+DELIMITER $$
+
+CREATE PROCEDURE done()
+BEGIN
+	
+	SELECT todo,priority,context,project,date_completed
+	FROM done
+	ORDER BY date_completed DESC
+	LIMIT 20;
+
+END;
+
+$$
+
+DELIMITER ;
